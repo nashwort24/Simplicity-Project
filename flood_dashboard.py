29eb27c7b527c3d78d39e886513a50bc6f749008
Your Name <you@example.com>
2026-02-23 15:00:30 -0600

    bug updates

--- flood_dashboard.py
+++ flood_dashboard.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import pickle
 import os
 
-app = Flask(__name__, template_folder='website')
+app = Flask(__name__, template_folder='Website', static_folder='Website')
 
 # Load merged data and train model
 # Use relative path so it works on deployment
